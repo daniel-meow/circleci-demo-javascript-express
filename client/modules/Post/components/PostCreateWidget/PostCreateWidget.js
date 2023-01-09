c045f442ba1537b3d3ce1599763c3643b1317a25
daniel <daniel@worldssportsgroup.com>
2023-01-09 10:47:38 +0700

    daniel edit

--- client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
+++ client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
@@ -1,8 +1,8 @@
-import React, { Component, PropTypes } from 'react';
-import { injectIntl, intlShape, FormattedMessage } from 'react-intl';
+import React, { Component, PropTypes } from "react";
+import { injectIntl, intlShape, FormattedMessage } from "react-intl";
 
 // Import Style
-import styles from './PostCreateWidget.css';
+import styles from "./PostCreateWidget.css";
 
 export class PostCreateWidget extends Component {
   addPost = () => {
@@ -11,20 +11,41 @@ export class PostCreateWidget extends Component {
     const contentRef = this.refs.content;
     if (nameRef.value && titleRef.value && contentRef.value) {
       this.props.addPost(nameRef.value, titleRef.value, contentRef.value);
-      nameRef.value = titleRef.value = contentRef.value = '';
+      nameRef.value = titleRef.value = contentRef.value = "";
     }
   };
 
   render() {
-    const cls = `${styles.form} ${(this.props.showAddPost ? styles.appear : '')}`;
+    const cls = `${styles.form} ${this.props.showAddPost ? styles.appear : ""}`;
     return (
       <div className={cls}>
-        <div className={styles['form-content']}>
-          <h2 className={styles['form-title']}><FormattedMessage id="createNewPost" /></h2>
-          <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} ref="name" />
-          <input placeholder={this.props.intl.messages.postTitle} className={styles['form-field']} ref="title" />
-          <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} ref="content" />
-          <a className={styles['post-submit-button']} href="#" onClick={this.addPost}><FormattedMessage id="submit" /></a>
+        <div className={styles["form-content"]}>
+          <h1>This got added by Daniel</h1>
+          <h2 className={styles["form-title"]}>
+            <FormattedMessage id="createNewPost" />
+          </h2>
+          <input
+            placeholder={this.props.intl.messages.authorName}
+            className={styles["form-field"]}
+            ref="name"
+          />
+          <input
+            placeholder={this.props.intl.messages.postTitle}
+            className={styles["form-field"]}
+            ref="title"
+          />
+          <textarea
+            placeholder={this.props.intl.messages.postContent}
+            className={styles["form-field"]}
+            ref="content"
+          />
+          <a
+            className={styles["post-submit-button"]}
+            href="#"
+            onClick={this.addPost}
+          >
+            <FormattedMessage id="submit" />
+          </a>
         </div>
       </div>
     );
